7749b650ef11ab22dc9f3e1a1a8a2f035e52bfd7
Karsten Reitan Sørensen <krs@aarhustech.dk>
2021-11-01 12:06:06 +0100

    Composer files installed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '70886be192c891650a194fdd47ee7576ae29edaa',
+        'name' => 'jetbrains/phpstorm-workshop',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jetbrains/phpstorm-workshop' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '70886be192c891650a194fdd47ee7576ae29edaa',
+            'dev_requirement' => false,
+        ),
+    ),
+);
